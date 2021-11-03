ee3cb5c79432a7349636909be9b6065def09638a
jaskirat0810 <jaskirat0810@gmail.com>
2021-11-04 00:12:42 +0530

    Adding Structure commit

---
+++ src/sentiment_analysis/dataloader.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from nltk.corpus import movie_reviews
+
+
+'''
+
+'''
+
+
+def load_data():
+    print(movie_reviews.categories())
+
+
+def get_processed_data():
+    load_data()
---
+++ src/sentiment_analysis/run.py
@@ -0,0 +1,9 @@
+from src.sentiment_analysis.dataloader import get_processed_data
+
+
+def run() -> None:
+    get_processed_data()
+
+
+if __name__ == "__main__":
+    run()
